6fa874a206627fd08728538884786eb849325e7b
kwa_laa <khoaddnguyen@gmail.com>
2023-09-13 16:00:02 -0500

    add image and gif, also add decorators for /bye

--- hello.py
+++ hello.py
@@ -6,9 +6,32 @@ print(__name__)
 
 @app.route("/")  # Python Decorator
 def hello_world():
-    return "Hello, World!"
+    return ("<h1 style='text-align: center'>Hello, World!</h1>"
+            "<p>This is a paragraph.</p>"
+            "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV7IwlGEQkFsd_NT-s6ySpDTVcnmzDYlhqG1GrqCcEAISWitfyn7wjRj-oR0ESU4pVW6w&usqp=CAU'>"
+            "</br>"
+            "<img src='https://media3.giphy.com/media/XtydbjSSwkC7K2zBTH/giphy.gif?cid=ecf05e478bo0w6jbwr87h10ie4puy0yi1t4mzwh5247qxvyo&ep=v1_gifs_gifId&rid=giphy.gif&ct=g' width=300>")
+
+def make_bold(function):
+    def wrapper_function():
+        return "<b>" + function() + "</b>"
+    return wrapper_function
+
+def make_emphasis(function):
+    def wrapper_function():
+        return "<em>" + function() + "</em>"
+    return wrapper_function
+
+def make_underline(function):
+    def wrapper_function():
+        return "<u>" + function() + "</u>"
+    return wrapper_function
+
 
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underline
 def say_bye():
     return "Bye"
 
